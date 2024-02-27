333e36aa2f578fad108e1d2cda7e81a63cf647f5
aitor <aitor@DESKTOP-GIR2RBG>
2024-02-27 14:21:44 +0100

    Codigo de los fragments(a completar cuando hagamos los models)

--- app/src/main/java/org/cuatrovientos/blablacar/activities/MainActivity.java
+++ app/src/main/java/org/cuatrovientos/blablacar/activities/MainActivity.java
@@ -16,10 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-    }
-
-    private void setup() {
+        //los distintos activities
 
     }
 }
