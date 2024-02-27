914b2fe83dadadb2e8f26c6800800ec18211e9f8
byronnDev <mikel.e.g.2004@gmail.com>
2024-02-27 13:51:29 +0100

    Login with email and pass, connected to firebase

--- app/src/main/java/org/cuatrovientos/blablacar/activities/Login.java
+++ app/src/main/java/org/cuatrovientos/blablacar/activities/Login.java
@@ -1,28 +1,83 @@
 package org.cuatrovientos.blablacar.activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.cuatrovientos.blablacar.R;
 
 public class Login extends AppCompatActivity {
-    Button LogOn;
+    Button login;
+    Button register;
+    EditText txtUser;
+    EditText txtPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        LogOn = (Button) findViewById(R.id.LoginBtnLogin);
+        login = (Button) findViewById(R.id.LoginBtnLogin);
+        register = (Button) findViewById(R.id.LoginBtnRegister);
+        txtUser = (EditText) findViewById(R.id.LoginTxtUsuario);
+        txtPass = (EditText) findViewById(R.id.LoginTxtContrasena);
 
-        LogOn.setOnClickListener(new View.OnClickListener() {
+        setup();
+    }
+
+    private void setup() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goHome();
+                if (areValidFields()){
+                    authUserAndAddToDatabase(txtUser.getText().toString(), txtPass.getText().toString());
+                }
             }
         });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (areValidFields()) {
+                    authUserWithDatabase(txtUser.getText().toString(), txtPass.getText().toString());
+                }
+            }
+        });
+    }
+
+    private void authUserWithDatabase(String user, String pass) {
+        FirebaseAuth.getInstance()
+                .signInWithEmailAndPassword(user, pass).addOnCompleteListener(command -> {
+            if (command.isSuccessful()) {
+                goHome();
+            } else {
+                showAlert();
+            }
+        });
+    }
+
+    private void authUserAndAddToDatabase(String user, String pass) {
+        FirebaseAuth.getInstance()
+                .createUserWithEmailAndPassword(user, pass).addOnCompleteListener(command -> {
+            if (command.isSuccessful()) {
+                goHome();
+            } else {
+                showAlert();
+            }
+        });
+    }
+
+    private boolean areValidFields() {
+        return !txtUser.getText().toString().isEmpty() && !txtPass.getText().toString().isEmpty();
+    }
+
+    private void Error(String message) {
+        Toast.makeText(getApplicationContext(),message, Toast.LENGTH_LONG).show();
     }
 
     private void goHome() {
@@ -35,4 +90,23 @@ public class Login extends AppCompatActivity {
         finish();
     }
 
+    private class GoogleProvider {
+        public GoogleProvider() {
+
+        }
+    }
+
+    private class AppleProvider {
+        public AppleProvider(){
+        }
+    }
+
+    private void showAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Error");
+        builder.setMessage("Se ha producido un error autenticando el usuario");
+        builder.setPositiveButton("Aceptar", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 }
--- app/src/main/java/org/cuatrovientos/blablacar/activities/MainActivity.java
+++ app/src/main/java/org/cuatrovientos/blablacar/activities/MainActivity.java
@@ -19,5 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void setup() {
 
+    }
 }
