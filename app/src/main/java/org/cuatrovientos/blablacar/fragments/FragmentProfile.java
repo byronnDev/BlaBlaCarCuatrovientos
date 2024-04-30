c0f224ab7f2bd7077c3a5582e72ec6c46f4c1f4c
byronnDev <mikel.e.g.2004@gmail.com>
2024-04-30 21:31:41 +0200

    Añadida funcionalidad Seleccionar imagen al clicar la del usuario y obtener datos de la bbdd (en desarrollo)

--- app/src/main/java/org/cuatrovientos/blablacar/fragments/FragmentProfile.java
+++ app/src/main/java/org/cuatrovientos/blablacar/fragments/FragmentProfile.java
@@ -1,16 +1,19 @@
 package org.cuatrovientos.blablacar.fragments;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,13 +31,16 @@ public class FragmentProfile extends Fragment {
     Button btnLogout;
     private TextView nombre;
     private TextView email;
+    private ImageView imgUsuario;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private TextView o2Points;
+
     FirebaseFirestore db;
     Map<String, Object> user = new HashMap<>();
     public FragmentProfile() {
         // Required empty public constructor
         db = FirebaseFirestore.getInstance();
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -44,20 +50,38 @@ public class FragmentProfile extends Fragment {
         //el nopmbre del usuario se obtiene del mail por un substring
         nombre = view.findViewById(R.id.txtNombre);
         email = view.findViewById(R.id.txtMail);
+        imgUsuario = view.findViewById(R.id.imgUsuario);
+        o2Points = view.findViewById(R.id.txtO2Points);
 
-        // Se obtiene el usuario de la base de datos
-        dbQuery dbQuery = new dbQuery(requireContext());
-        String dbMail = dbQuery.getCurrentUserEmail();
-
-        email.setText(dbMail);
-        db.collection("users").document(dbMail)
-                .get().addOnSuccessListener(v -> {
-                    if (v.exists()) {
-                        user = v.getData();
-                        nombre.setText(user.get("name").toString());
+        // TODO No está pillando los datos del usuario ni actualizándo la imagen
+        db.collection("users").document(FirebaseAuth.getInstance().getCurrentUser().getEmail())
+                .get().addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        user = documentSnapshot.getData();
+                        if (user != null) {
+                            email.setText(user.containsKey("mail") ? user.get("mail").toString() : "");
+                            String nombreYApellido = (user.containsKey("name") ? user.get("name").toString() : "") + " " +
+                                    (user.containsKey("surname") ? user.get("surname").toString() : "");
+                            nombre.setText(nombreYApellido);
+                            if (user.containsKey("picture")) {
+                                String imgUrl = user.get("picture").toString();
+                                if (imgUrl != null) {
+                                    imgUsuario.setImageURI(Uri.parse(imgUrl));
+                                }
+                            }
+                            o2Points.setText(user.containsKey("o2Points") ? user.get("o2Points").toString() : "");
+                        }
                     }
                 });
-
+        imgUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent para abrir la galería de imágenes
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Selecciona una imagen"), PICK_IMAGE_REQUEST);
+            }
+        });
 
         btnLogout = (Button) view.findViewById(R.id.btnLogOut);
         onLogout();
