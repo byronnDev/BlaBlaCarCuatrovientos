9a3da364663c596a4ff6e5d3e2e8023b574ea994
aitor <aitor@DESKTOP-GIR2RBG>
2024-05-10 10:47:00 +0200

    Clase Registrar terminada

--- app/src/main/java/org/cuatrovientos/blablacar/activities/Registrar.java
+++ app/src/main/java/org/cuatrovientos/blablacar/activities/Registrar.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,28 +21,32 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.cuatrovientos.blablacar.R;
 import org.cuatrovientos.blablacar.models.Route;
+import org.cuatrovientos.blablacar.models.User;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import io.realm.Realm;
+import io.realm.RealmResults;
+
 public class Registrar extends AppCompatActivity {
-    private FirebaseFirestore db;
-    private TextView txtName;
-    private TextView txtSurname;
-    private TextView txtMail;
-    private TextView txtPass;
-    private TextView txtPhone;
+    private EditText txtName;
+    private EditText txtSurname;
+    private EditText txtMail;
+    private EditText txtPass;
+    private EditText txtPhone;
     private Button btnRegister;
+    Realm realm;
+    RealmResults<User> realmList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+        realm = Realm.getDefaultInstance();
         setup();
-
-        db = FirebaseFirestore.getInstance(); // Inicializar Firestore
-
         onRegister();
     }
 
@@ -53,6 +58,19 @@ public class Registrar extends AppCompatActivity {
             }
         });
     }
+    private void registerUser() {
+        String name = txtName.getText().toString();
+        String surname = txtSurname.getText().toString();
+        String mail = txtMail.getText().toString();
+        String pass = txtPass.getText().toString();
+        String phone = txtPhone.getText().toString();
+        User user = new User(name,surname,mail,pass,phone);
+        realm.beginTransaction();
+        realm.copyToRealm(user);
+        realm.commitTransaction();
+        realm.close();
+        goHome();
+    }
 
     private void goHome() {
         Intent intent = new Intent(this, MainActivity.class);
@@ -61,15 +79,15 @@ public class Registrar extends AppCompatActivity {
     }
 
     private boolean isInputValid() {
-        return !TextUtils.isEmpty(txtName.getText())
-                && !TextUtils.isEmpty(txtSurname.getText())
-                && isEmailValid(txtMail.getText())
-                && isPasswordValid(txtPass.getText())
-                && isPhoneValid(txtPhone.getText());
+        return !TextUtils.isEmpty(txtName.getText().toString())
+                && !TextUtils.isEmpty(txtSurname.getText().toString())
+                && isEmailValid(txtMail.getText().toString())
+                && isPasswordValid(txtPass.getText().toString())
+                && isPhoneValid(txtPhone.getText().toString());
     }
 
-    private boolean isPhoneValid(CharSequence phone) {
-        if (phone.toString().isEmpty()) {
+    private boolean isPhoneValid(String phone) {
+        if (phone.isEmpty()) {
             showErrorToast("Por favor, introduce un número de teléfono.");
             return false;
         }
@@ -82,12 +100,16 @@ public class Registrar extends AppCompatActivity {
         return true;
     }
 
-    private boolean isEmailValid(CharSequence email) {
-        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    private boolean isEmailValid(String email) {
+        User user = realm.where(User.class).equalTo("mail", email).findFirst();
+        if (user != null){
+            return true;
+        }
+        return false;
     }
 
-    private boolean isPasswordValid(CharSequence password) {
-        if (password.toString().isEmpty()) {
+    private boolean isPasswordValid(String password) {
+        if (password.isEmpty()) {
             showErrorToast("Por favor, introduce una contraseña.");
             return false;
         }
@@ -98,64 +120,27 @@ public class Registrar extends AppCompatActivity {
         }
 
         // Caracter especial, número y mayúscula
-        if (!password.toString().matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$")) {
+        if (!password.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$")) {
             showErrorToast("La contraseña debe tener al menos un número, una mayúscula y un carácter especial.");
+            return false;
+        }
+
+        if (password.contains(" ")){
+            showErrorToast("La contraseña no puede neten espacios en blanco.");
             return false;
         }
 
         return true;
     }
 
-    private void registerUser() {
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(txtMail.getText().toString(), txtPass.getText().toString())
-                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
-                        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-                        if (currentUser != null) {
-                            addUserToDatabase();
-                            goHome();
-                        } else {
-                            showErrorToast("Error al registrar el usuario.");
-                        }
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        showErrorToast("Error al crear la cuenta: " + e.getMessage());
-                    }
-                });
-    }
-
-    private void addUserToDatabase() {
-        Map<String, Object> user = new HashMap<>();
-        user.put("name", txtName.getText().toString());
-        user.put("surname", txtSurname.getText().toString());
-        user.put("mail", txtMail.getText().toString());
-        user.put("phone", txtPhone.getText().toString());
-        // Start with 0 O2 points and an empty list of routes
-        user.put("O2Points", 0);
-        user.put("Routes", new ArrayList<Route>());
-        user.put("routesSubscribed", new ArrayList<Route>());
-        user.put("routesBaned", new ArrayList<Route>());
-
-        db.collection("users").document(txtMail.getText().toString()).set(user)
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        showErrorToast("Error al guardar los datos del usuario en la base de datos: " + e.getMessage());
-                    }
-                });
-    }
 
     private void setup() {
-        txtName = findViewById(R.id.txtName);
-        txtSurname = findViewById(R.id.txtSurname);
-        txtMail = findViewById(R.id.txtMail);
-        txtPass = findViewById(R.id.txtPass);
-        txtPhone = findViewById(R.id.txtPhone);
-        btnRegister = findViewById(R.id.btnRegister);
+        txtName = (EditText) findViewById(R.id.txtName);
+        txtSurname = (EditText) findViewById(R.id.txtSurname);
+        txtMail = (EditText) findViewById(R.id.txtMail);
+        txtPass = (EditText) findViewById(R.id.txtPass);
+        txtPhone = (EditText) findViewById(R.id.txtPhone);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
     }
 
     private void showErrorToast(String message) {
