cd47b6a45a9f545226cdd55995b875660b9054f5
byronnDev <mikel.e.g.2004@gmail.com>
2024-05-13 22:26:33 +0200

    Cargar DummyData para pruebas

--- app/src/main/java/org/cuatrovientos/blablacar/utils/Utils.java
+++ app/src/main/java/org/cuatrovientos/blablacar/utils/Utils.java
@@ -4,18 +4,16 @@ import org.cuatrovientos.blablacar.models.User;
 
 import java.util.ArrayList;
 
+import io.realm.Realm;
 
 
 public class Utils {
     public static ArrayList<User> getDummyData() {
-        ArrayList userList = new ArrayList();
-
-        userList.add(new User("test1","testo","test1@gmail.com","Qwerty123.","344745098"));
-        userList.add(new User("test2","testo","test2@gmail.com","Qwerty123.","344567094"));
-        userList.add(new User("test3","testo","test3@gmail.com","Qwerty123.","347973294"));
-
-
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(new User("test","testo","test@test.com","Qwerty123.","344567094"));
+        userList.add(new User("test1","testo","test1@test.com","Qwerty123.","344745098"));
+        userList.add(new User("test2","testo","test2@test.com","Qwerty123.","344567094"));
+        userList.add(new User("test3","testo","test3@test.com","Qwerty123.","347973294"));
         return userList;
-
     }
 }
