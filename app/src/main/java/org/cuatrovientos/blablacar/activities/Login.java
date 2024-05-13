a00fd2a4a81a0acf7b644bfc7fed3f61f9727af1
byronnDev <mikel.e.g.2004@gmail.com>
2024-05-13 22:24:16 +0200

    Cargar el dummyData y subir al realm, quitar todo lo de FirebaseAuth, y corregir el showAlert

--- app/src/main/java/org/cuatrovientos/blablacar/activities/Login.java
+++ app/src/main/java/org/cuatrovientos/blablacar/activities/Login.java
@@ -1,5 +1,7 @@
 package org.cuatrovientos.blablacar.activities;
 
+import static org.cuatrovientos.blablacar.utils.Utils.getDummyData;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +15,11 @@ import android.widget.EditText;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.cuatrovientos.blablacar.R;
+import org.cuatrovientos.blablacar.models.LoguedUser;
+import org.cuatrovientos.blablacar.models.User;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class Login extends AppCompatActivity {
     private ActivityResultLauncher<Intent> launcher;
@@ -20,6 +27,8 @@ public class Login extends AppCompatActivity {
     Button register;
     EditText txtUser;
     EditText txtPass;
+    Realm realm;
+    RealmResults<User> userList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,9 +38,28 @@ public class Login extends AppCompatActivity {
         txtUser = (EditText) findViewById(R.id.txtMail);
         txtPass = (EditText) findViewById(R.id.txtPass);
 
+        realm = Realm.getDefaultInstance();
+
+//        purgeData(); // TODO: Eliminar esta línea en producción
+        chargeDummy();
+
         setupSignInButtons(); // Configura los botones de inicio de sesión
     }
+    private void chargeDummy() {
+        userList = realm.where(User.class).findAll();
+        if (userList.size() == 0) {
+            realm.beginTransaction();
+            realm.copyToRealm(getDummyData());
+            realm.commitTransaction();
+        }
+        realm.close();
+    }
 
+    private void purgeData() {
+        realm.beginTransaction();
+        realm.deleteAll();
+        realm.commitTransaction();
+    }
     private void setupSignInButtons() {
         // Botones inicio sesión
         setRegisterFunction();
@@ -55,52 +83,45 @@ public class Login extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(Login.this, Registrar.class);
                 startActivity(intent);
-                finish();
-//                if (areValidFields()){
-//                    authUserAndAddToDatabase(txtUser.getText().toString(), txtPass.getText().toString());
-//                }
             }
         });
     }
 
-    private void authUserWithDatabase(String user, String pass) {
-        FirebaseAuth.getInstance()
-                .signInWithEmailAndPassword(user, pass).addOnCompleteListener(command -> {
-            if (command.isSuccessful()) {
-                goHome(txtUser.getText().toString());
+    private void authUserWithDatabase(String userEmail, String password) {
+        // Abrir una instancia de Realm
+        Realm realm = Realm.getDefaultInstance();
+        try {
+            // Buscar el usuario por email
+            User user = realm.where(User.class).equalTo("mail", userEmail).findFirst();
+            if (user != null && user.getPass().equals(User.hashPassword(password))) {
+                // Guardar el usuario logueado
+                LoguedUser.setUser(user);
+                // Si el usuario existe y la contraseña es correcta
+                goHome();
             } else {
-                showAlert();
+                // Mostrar alerta si el usuario no existe o la contraseña es incorrecta
+                showAlert("Error", "Usuario o contraseña incorrectos.");
             }
-        });
-    }
-
-    private void authUserAndAddToDatabase(String user, String pass) {
-        FirebaseAuth.getInstance()
-                .createUserWithEmailAndPassword(user, pass).addOnCompleteListener(command -> {
-            if (command.isSuccessful()) {
-                goHome(txtUser.getText().toString());
-            } else {
-                showAlert();
-            }
-        });
+        } finally {
+            // Asegúrate de cerrar Realm en el hilo actual
+            realm.close();
+        }
     }
 
     private boolean areValidFields() {
         return !txtUser.getText().toString().isEmpty() && !txtPass.getText().toString().isEmpty();
     }
 
-    private void goHome(String email) {
+    private void goHome() {
         Intent intent = new Intent(Login.this, MainActivity.class);
-        // Start the new activity
         startActivity(intent);
-        // Finish the current activity
         finish();
     }
 
-    private void showAlert() {
+    private void showAlert(String title, String message) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Error");
-        builder.setMessage("Se ha producido un error autenticando el usuario");
+        builder.setTitle(title);
+        builder.setMessage(message);
         builder.setPositiveButton("Aceptar", null);
         AlertDialog dialog = builder.create();
         dialog.show();
