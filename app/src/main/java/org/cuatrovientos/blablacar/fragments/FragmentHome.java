6c7708ae8d188f3af55a220bac2f44023e29e3f8
byronnDev <mikel.e.g.2004@gmail.com>
2024-05-16 00:52:34 +0200

    Añadida funcion al boton de buscar, y enviar fecha al fragment de addroutes

--- app/src/main/java/org/cuatrovientos/blablacar/fragments/FragmentHome.java
+++ app/src/main/java/org/cuatrovientos/blablacar/fragments/FragmentHome.java
@@ -2,83 +2,97 @@ package org.cuatrovientos.blablacar.fragments;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-
 import org.cuatrovientos.blablacar.R;
-
 import java.util.Calendar;
 
 public class FragmentHome extends Fragment {
-    EditText etPlannedDate;
+    public EditText etPlannedDate;
+    Button btnFind;
 
     public FragmentHome() {
+    }
+
+    // Define una interfaz para comunicar la fecha seleccionada al Activity principal
+    public interface OnDateSelectedListener {
+        void onDateSelected(String date);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        etPlannedDate = (EditText) view.findViewById(R.id.etPlannedDate);
+        etPlannedDate = view.findViewById(R.id.etPlannedDate);
+        btnFind = view.findViewById(R.id.btnFind);
 
-        // Define click listener for etPlannedDate
         etPlannedDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (v.getId() == R.id.etPlannedDate) {
-                    // Create a new DatePickerFragment instance
-                    DialogFragment newFragment = new DatePickerFragment();
-                    newFragment.show(getParentFragmentManager(), "datePicker");
-                }
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getChildFragmentManager(), "datePicker");
             }
         });
+
+        btnFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear una instancia del FragmentAddRoutes
+                FragmentAddRoutes fragmentAddRoutes = new FragmentAddRoutes();
+
+                // Pasar la fecha seleccionada como un argumento al FragmentAddRoutes
+                Bundle bundle = new Bundle();
+                bundle.putString("selectedDate", etPlannedDate.getText().toString());
+                // TODO: Pasar el resto de los datos necesarios
+                fragmentAddRoutes.setArguments(bundle);
+
+                // Realizar la transacción del fragmento
+                getParentFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.defaultView, fragmentAddRoutes)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
         return view;
     }
 
-    // Define DatePickerFragment class outside the listener
+    // Clase DatePickerFragment
     public static class DatePickerFragment extends DialogFragment
             implements DatePickerDialog.OnDateSetListener {
 
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current date as the default date in the picker
             final Calendar c = Calendar.getInstance();
             int year = c.get(Calendar.YEAR);
             int month = c.get(Calendar.MONTH);
             int day = c.get(Calendar.DAY_OF_MONTH);
-
-            // Create a new instance of DatePickerDialog and return it
-            return new DatePickerDialog(getActivity(), this, year, month, day);
+            return new DatePickerDialog(requireActivity(), this, year, month, day);
         }
 
         public void onDateSet(DatePicker view, int year, int month, int day) {
-            // Do something with the date chosen by the user
-            // Example: Update etPlannedDate text in FragmentHome
             FragmentHome fragmentHome = (FragmentHome) getParentFragment();
             if (fragmentHome != null) {
-                fragmentHome.etPlannedDate.setText(day + " / " + (month + 1) + " / " + year);
+                String selectedDate = day + " / " + (month + 1) + " / " + year;
+                fragmentHome.onDateSelected(selectedDate);
+                fragmentHome.etPlannedDate.setText(selectedDate);
             }
         }
     }
 
-
-
-    public void renderData() {
-        //TODO
-        //crear un objeto de tipo Ruta y construirlo, luego el resto de la logica de la clase
-
-
-
-
-
+    // Método para enviar la fecha seleccionada al Activity principal
+    private void onDateSelected(String date) {
+        if (getActivity() instanceof OnDateSelectedListener) {
+            ((OnDateSelectedListener) getActivity()).onDateSelected(date);
+        }
     }
 }
