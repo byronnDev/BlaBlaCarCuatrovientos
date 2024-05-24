e26d8335b27e120a3c08d1d4d933ca89a08a11f0
byronnDev <mikel.e.g.2004@gmail.com>
2024-05-24 03:56:17 +0200

    Mostrar hora de ruta en el item

--- app/src/main/java/org/cuatrovientos/blablacar/adapters/RecyclerDataAdapter.java
+++ app/src/main/java/org/cuatrovientos/blablacar/adapters/RecyclerDataAdapter.java
@@ -8,35 +8,32 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.FirebaseFirestore;
-
 import org.cuatrovientos.blablacar.R;
 import org.cuatrovientos.blablacar.models.Route;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 import io.realm.RealmList;
 
-public class RecyclerDataAdapter extends RecyclerView.Adapter<RecyclerDataAdapter.RecyclerDataHolder>{
+public class RecyclerDataAdapter extends RecyclerView.Adapter<RecyclerDataAdapter.RecyclerDataHolder> {
     List<Route> listaDeElementos;
     OnItemClickListener itemListener;
 
     public RecyclerDataAdapter(RealmList<String> usuariosApuntados) {
-
+        // Constructor vacío, no utilizado en este contexto
     }
 
-    public RecyclerDataAdapter(List<Route> lista, OnItemClickListener listener){
+    public RecyclerDataAdapter(List<Route> lista, OnItemClickListener listener) {
         listaDeElementos = lista;
         itemListener = listener;
     }
 
-
-
     @NonNull
     @Override
     public RecyclerDataAdapter.RecyclerDataHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.routes_item,null,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.routes_item, null, false);
         return new RecyclerDataHolder(view);
     }
 
@@ -51,35 +48,38 @@ public class RecyclerDataAdapter extends RecyclerView.Adapter<RecyclerDataAdapte
     }
 
     public class RecyclerDataHolder extends RecyclerView.ViewHolder {
-    TextView tvLugarInicio;
-    TextView tvLugarFin;
-    TextView tvHuecos;
-    TextView tvHoraSalida;
+        TextView tvLugarInicio;
+        TextView tvLugarFin;
+        TextView tvHuecos;
+        TextView tvHoraSalida;
 
-    public RecyclerDataHolder(@NonNull View itemView) {
-        super(itemView);
-        tvLugarInicio = itemView.findViewById(R.id.tvLugarInicio);
-        tvLugarFin = itemView.findViewById(R.id.tvLugarFin);
-        tvHuecos = itemView.findViewById(R.id.tvHuecos);
-        tvHoraSalida = itemView.findViewById(R.id.tvHoraSalida);
-    }
-    public void assignData(Route route, OnItemClickListener listener) {
-        tvLugarInicio.setText(tvLugarInicio.getText() + route.getLugarInicio());
-        tvLugarFin.setText(tvLugarFin.getText() + route.getLugarFin());
-        tvHuecos.setText(tvHuecos.getText() + String.valueOf(route.getHuecos()));
-        //TODO setear vien, lo que regresa es un Date
-        //tvHoraSalida.setText(route.getHoraSalida());
-        itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                listener.onItemClick(route);
-            }
-        });
-    }
+        public RecyclerDataHolder(@NonNull View itemView) {
+            super(itemView);
+            tvLugarInicio = itemView.findViewById(R.id.tvLugarInicio);
+            tvLugarFin = itemView.findViewById(R.id.tvLugarFin);
+            tvHuecos = itemView.findViewById(R.id.tvHuecos);
+            tvHoraSalida = itemView.findViewById(R.id.tvHoraSalida);
+        }
+
+        public void assignData(Route route, OnItemClickListener listener) {
+            tvLugarInicio.setText(route.getLugarInicio());
+            tvLugarFin.setText(route.getLugarFin());
+            tvHuecos.setText(String.valueOf(route.getHuecos()));
+
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.getDefault());
+            String formattedTime = sdf.format(route.getHoraSalida());
+            tvHoraSalida.setText(formattedTime);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(route);
+                }
+            });
+        }
     }
 
     public interface OnItemClickListener {
-        void onItemClick(Route conten);
-
+        void onItemClick(Route route);
     }
 }
